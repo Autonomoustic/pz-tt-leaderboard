eac124a9569257edf3b18ea6d7bad48121140ea3
Matthew Garfitt <matthew@payzilch.com>
2019-09-02 19:31:12 +0100

    add table score

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Table from './containers/table/Table';
 import './App.css';
 
--- src/containers/table/Table.js
+++ src/containers/table/Table.js
@@ -1,10 +1,36 @@
+import React from 'react';
+import Person from '../../components/person';
 
-class Table extends Component {
+class Table extends React.Component {
+    state = {
+        loading: false,
+        people: []
+    }
+
+    componentDidMount() {
+        const requestUrl = 'https://api.sheety.co/36ce2504-c477-494c-9578-4d00e63ef165';
+        this.setState({loading: true});
+        fetch(requestUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                this.setState({people: response, loading: false});
+        })
+
+
+    }
+
     render() {
+        const renderPeople = this.state.people ? this.state.people.map((person, index) => {
+                return <Person key={index} person={person}/>
+            }) : null
         return (
-            <Aux>
-                <Table />
-            </Aux>
+            <div>
+                {renderPeople}
+            </div>
         );
     }
 }
+
+export default Table;
